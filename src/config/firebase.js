971001b76aa69c723f5488447683edbf3b4068cf
Cindy Y. Caceres P. <cindycaceres134@gmail.com>
2026-05-21 07:31:24 -0500

    feat: firebase auth 2

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,9 +9,9 @@ from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCjYadHmZosE3etO6fq_G1CT4RLbOwOBhI",
-  authDomain: "santobocado-api.firebaseapp.com",
-  projectId: "santobocado-api",
+  apiKey:  import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "santobocado-api.firebasestorage.app",
   messagingSenderId: "676670110523",
   appId: "1:676670110523:web:f25d36ceaa522ca121d51e",
